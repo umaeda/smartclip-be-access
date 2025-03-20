2bbfaa24281c3057fd8bea040ad7e61e0c943be7
ulisses.maeda <ulisses.maeda@gmail.com>
2025-03-20 00:57:13 -0300

    add 2

--- function_app.py
+++ function_app.py
@@ -1,10 +1,5 @@
 import azure.functions as func
-from mangum import Mangum
-from main import app as fastapi_app
 
-# Create a Mangum handler to properly handle the routing
-# This will help avoid the double slash issue
-handler = Mangum(fastapi_app, lifespan="off")
+from x import app as fastapi_app
 
-# Create the Azure Functions app
-app = func.AsgiFunctionApp(app=handler, http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
