8d7d5b572180e53611777aa3fd621b7a045c2398
ulisses.maeda <ulisses.maeda@gmail.com>
2025-04-11 10:55:19 -0300

    .

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ from app.core.logger import app_logger
 #     import uvicorn
 #     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True, log_level="info")
 #     app_logger.info("SmartClip API server stopped")
+
+#if __name__ == "__main__":
+#    import uvicorn
+#    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
     
